2aa7fed140a84f4872964101db1a0db04ea77d56
Michel <michel.wahmes@t-online.de>
2021-10-29 13:39:19 +0200

    fakultät geht

--- src/ezahl.java
+++ src/ezahl.java
@@ -1,5 +1,20 @@
 public class ezahl {
     public static void main(String[] args) {
-        System.out.print("moijn");
+        System.out.println("moijn");
+        int a = factorial(6);
+        System.out.print(a);
+
+    }
+    public static double a {
+
+    }
+
+    public static int factorial(int n){
+        if(n <= 1){
+            return 1;
+        }
+        else {
+            return n*factorial(n-1);
+        }
     }
 }
